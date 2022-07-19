ee81b7261870e5d65cf1204d03ed92b3344b40e8
Tobias Effner <tobias.effner@rackspace.com>
2022-07-19 09:36:16 +0200

    Added logging

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import argparse
 import json
 import time
+import logging
 
 import adafruit_dht
 import board
 import paho.mqtt.publish as publish
+import os
 
 
 def send_message(message):
     publish.single(topic="office/sensor1", hostname=mqtt_hostname, port=1883, auth=mqtt_auth, payload=message)
-    print(f'Message sent: {message}')
+    logging.info(f'Message sent: {message}')
 
 
 def get_temp():
@@ -23,6 +25,7 @@ def get_temp():
             "humidity": humidity
         })
     except RuntimeError:
+        logging.debug('Could not get any data from the sensor.')
         return None
 
 
@@ -41,10 +44,16 @@ if len(args.mqtt) == 3:
 # initial device
 dhtDevice = adafruit_dht.DHT22(board.D2)
 
+script_path = os.path.dirname(os.path.realpath(__file__))
+logging.basicConfig(filename=f'{script_path}/metrics_sender.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
+logging.info(f'Starting metrics sender with: {mqtt_hostname}')
 
-if __name__ == "__main__":
-    while True:
-        json_message = get_temp()
-        if json_message is not None:
-            send_message(json_message)
-        time.sleep(15)
+while True:
+    json_message = get_temp()
+    if json_message is not None:
+        send_message(json_message)
+    time.sleep(15)
